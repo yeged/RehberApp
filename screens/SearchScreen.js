388dfc83315d1e4fa56c4ace3431e8e326cbdd61
yeged <yigityceylan@gmail.com>
2020-04-01 20:28:56 +0300

    Adding Default Title Component

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -2,13 +2,27 @@ import React from "react";
 import { View,Text,StyleSheet,Button } from "react-native";
 
 import SearchEngine from "../components/SearchEngine"
+import DefaultTitle from "../components/DefaultTitle"
 
 
 const SearchScreen = (props) => {
     return(
-            <SearchEngine />     
+            <View style={styles.screen}>
+            <SearchEngine />
+            <View style= {styles.headerContainer}>
+            <DefaultTitle>DENEME</DefaultTitle>
+            </View>
+            </View>     
     )
 }
 
+const styles = StyleSheet.create({
+    screen:{
+        flex:1,
+    },
+    headerContainer:{
+        alignItems:"center",
+    }
+})
 
 export default SearchScreen;
